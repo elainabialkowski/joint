aef0e199bebeb169209baf5d998cb178d98f78cb
Elaina Bialkowski <elaina@elainabialkowski.dev>
2022-06-02 17:07:15 -0700

    moved plaid api requests to plaid.go

--- main.go
+++ main.go
@@ -3,21 +3,13 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
-	"github.com/plaid/plaid-go/v3/plaid"
 )
 
 func main() {
-
-	plaidconfig := plaid.NewConfiguration()
-	plaidconfig.AddDefaultHeader("PLAID-CLIENT-ID", os.Getenv("PLAID_CLIENT_ID"))
-	plaidconfig.AddDefaultHeader("PLAID-SECRET", os.Getenv("PLAID_CLIENT_SECRET"))
-	plaidconfig.UseEnvironment(plaid.Sandbox)
-	plaidclient := plaid.NewAPIClient(plaidconfig)
 
 	db, err := pgx.Connect(context.Background(), os.Getenv("POSTGRES_URI"))
 	if err != nil {
@@ -26,53 +18,8 @@ func main() {
 	defer db.Close(context.Background())
 
 	r := gin.Default()
-
-	r.POST("/plaid/link/token", func(ctx *gin.Context) {
-		req := plaid.NewLinkTokenCreateRequest(
-			"Plaid Test App",
-			"en",
-			[]plaid.CountryCode{plaid.COUNTRYCODE_CA},
-			*plaid.NewLinkTokenCreateRequestUser("user_good"),
-		)
-		req.SetWebhook(os.Getenv("WEBHOOK_URI"))
-		req.SetRedirectUri(os.Getenv("REDIRECT_URI"))
-		req.SetProducts([]plaid.Products{plaid.PRODUCTS_AUTH})
-
-		resp, _, err := plaidclient.PlaidApi.LinkTokenCreate(ctx).Execute()
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"err": err.Error(),
-			})
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"link_token": resp.GetLinkToken(),
-		})
-	})
-
-	r.GET("/plaid/access/token", func(ctx *gin.Context) {
-		publicToken := ctx.PostForm("public_token")
-
-		req := plaid.NewItemPublicTokenExchangeRequest(publicToken)
-		resp, _, err := plaidclient.PlaidApi.
-			ItemPublicTokenExchange(ctx).
-			ItemPublicTokenExchangeRequest(
-				*req,
-			).Execute()
-
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"err": err.Error(),
-			})
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"access_token": resp.GetAccessToken(),
-			"item_id":      resp.GetItemId(),
-		})
-
-	})
-
+	r.POST("/plaid/link/token", createLinkToken)
+	r.GET("/plaid/access/token", getAccessToken)
 	r.Run()
 
 }
---
+++ plaid.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/plaid/plaid-go/v3/plaid"
+)
+
+var (
+	plaidconfig = &plaid.Configuration{
+		DefaultHeader: map[string]string{
+			"PLAID-CLIENT-ID": os.Getenv("PLAID_CLIENT_ID"),
+			"PLAID-SECRET":    os.Getenv("PLAID_CLIENT_SECRET"),
+		},
+	}
+	plaidclient = plaid.NewAPIClient(plaidconfig)
+)
+
+func createLinkToken(c *gin.Context) {
+	req := plaid.NewLinkTokenCreateRequest(
+		"Plaid Test App",
+		"en",
+		[]plaid.CountryCode{plaid.COUNTRYCODE_CA},
+		*plaid.NewLinkTokenCreateRequestUser("user_good"),
+	)
+	req.SetWebhook(os.Getenv("WEBHOOK_URI"))
+	req.SetRedirectUri(os.Getenv("REDIRECT_URI"))
+	req.SetProducts([]plaid.Products{plaid.PRODUCTS_AUTH})
+
+	resp, _, err := plaidclient.PlaidApi.LinkTokenCreate(c).Execute()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"link_token": resp.GetLinkToken(),
+	})
+}
+
+func getAccessToken(c *gin.Context) {
+	publicToken := c.PostForm("public_token")
+
+	req := plaid.NewItemPublicTokenExchangeRequest(publicToken)
+	resp, _, err := plaidclient.PlaidApi.
+		ItemPublicTokenExchange(c).
+		ItemPublicTokenExchangeRequest(
+			*req,
+		).Execute()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"access_token": resp.GetAccessToken(),
+		"item_id":      resp.GetItemId(),
+	})
+}
