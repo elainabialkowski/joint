d1ba5720682c660bff929bc229cfb13648f0d32e
Elaina Bialkowski <elaina@crowboat.dev>
2022-06-04 15:20:18 -0400

    added simple connection initializing.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5"
+	"github.com/elainabialkowski/joint/service"
 )
 
 func main() {
-
-	db, err := pgx.Connect(context.Background(), os.Getenv("POSTGRES_URI"))
-	if err != nil {
-		log.Fatalf("Could not connect to db: %s\n", err.Error())
-	}
-	defer db.Close(context.Background())
-
-	r := gin.Default()
-	r.Run()
-
+	service.Server{}.Run()
 }
--- service/server.go
+++ service/server.go
@@ -1,12 +1,49 @@
 package service
 
 import (
+	"context"
+	"os"
+
+	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5"
 )
 
 type Server struct {
-	Db       *pgx.ConnPool
+	Db       *pgx.Conn
 	Cache    *redis.Conn
 	Sessions *redis.Conn
+}
+
+func (srv Server) Run() error {
+
+	var err error
+	srv.Db, err = pgx.Connect(context.TODO(), os.Getenv("PG_URI"))
+	if err != nil {
+		return err
+	}
+
+	cache, err := redis.DialURLContext(context.Background(), os.Getenv("CACHE_URI"), redis.DialOption{})
+	if err != nil {
+		return err
+	}
+	srv.Cache = &cache
+
+	sessions, err := redis.DialURLContext(context.Background(), os.Getenv("SESSIONS_URI"), redis.DialOption{})
+	if err != nil {
+		return err
+	}
+	srv.Sessions = &sessions
+
+	plaidService := PlaidService{}.Initialize(&srv)
+	userService := UserService{}.Initialize(&srv)
+
+	router := gin.Default()
+	router.POST("/plaid/link/token", plaidService.CreateLinkToken)
+	router.GET("/plaid/access/token", plaidService.GetAccessToken)
+	router.GET("/user/:id", userService.GetUser)
+
+	router.Run(os.Getenv("PORT"))
+
+	return nil
 }
