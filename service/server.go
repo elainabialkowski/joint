3d7b16591af0c3f2cd6038d64b6ecc0d479a0fc0
Elaina Bialkowski <elaina@elainabialkowski.dev>
2022-06-04 16:20:03 -0700

    Changed redis connections to conn pools.

--- service/server.go
+++ service/server.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
@@ -12,8 +13,8 @@ import (
 
 type Server struct {
 	Db       *sqlx.DB
-	Cache    *redis.Conn
-	Sessions *redis.Conn
+	Cache    *redis.Pool
+	Sessions *redis.Pool
 }
 
 func (srv Server) Run() error {
@@ -23,25 +24,56 @@ func (srv Server) Run() error {
 	if err != nil {
 		return err
 	}
+	defer srv.Db.Close()
 
-	cache, err := redis.DialURLContext(context.Background(), os.Getenv("CACHE_URI"), redis.DialOption{})
-	if err != nil {
-		return err
+	srv.Cache = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		DialContext: func(ctx context.Context) (redis.Conn, error) {
+			c, err := redis.Dial("tcp", os.Getenv("CACHE_URI"))
+			if err != nil {
+				return nil, err
+			}
+			if _, err := c.Do("AUTH", os.Getenv("CACHE_PASSWORD")); err != nil {
+				c.Close()
+				return nil, err
+			}
+			if _, err := c.Do("SELECT", os.Getenv("CACHE_DB")); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, nil
+		},
 	}
-	srv.Cache = &cache
 
-	sessions, err := redis.DialURLContext(context.Background(), os.Getenv("SESSIONS_URI"), redis.DialOption{})
-	if err != nil {
-		return err
+	srv.Sessions = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		DialContext: func(ctx context.Context) (redis.Conn, error) {
+			c, err := redis.Dial("tcp", os.Getenv("SESSIONS_URI"))
+			if err != nil {
+				return nil, err
+			}
+			if _, err := c.Do("AUTH", os.Getenv("SESSIONS_PASSWORD")); err != nil {
+				c.Close()
+				return nil, err
+			}
+			if _, err := c.Do("SELECT", os.Getenv("SESSIONS_DB")); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, nil
+		},
 	}
-	srv.Sessions = &sessions
 
 	plaidService := PlaidService{}.Initialize(&srv)
 	userService := UserService{}.Initialize(&srv)
 
 	router := gin.Default()
+
 	router.POST("/plaid/link/token", plaidService.CreateLinkToken)
 	router.GET("/plaid/access/token", plaidService.GetAccessToken)
+
 	router.GET("/user/:id", userService.GetUser)
 
 	router.Run(os.Getenv("PORT"))
